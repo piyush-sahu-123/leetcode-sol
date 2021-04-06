b117d7e521f859965ea6a7a79f0a4e7496ae41e1
Piyush Sahu <59291227+piyush-sahu-123@users.noreply.github.com>
2021-04-06 16:26:51 +0530

    Delete single-number directory

--- single-number/single-number.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int bro=0;
-        for(int i=0;i<nums.size();i++)
-            bro=bro^nums[i];
-        return bro;
-    }
-};
